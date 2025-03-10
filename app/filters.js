f3055b5b7334f921e114557734f2e976557d9749
Angeltapes <bobby.king@madetech.com>
2025-03-10 09:03:13 +0000

    Added a filter, polished D3 chart

--- app/filters.js
+++ app/filters.js
@@ -8,3 +8,10 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+//adds commas to separate thousands, for numbers greater than 999
+addFilter('addcommas', function (content) {
+    return content.toLocaleString("en-US") 
+  })
+
+    //TO ADD - case to handle nothing being there   
+    
