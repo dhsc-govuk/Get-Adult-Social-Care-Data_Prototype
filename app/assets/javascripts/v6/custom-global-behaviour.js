eedd19bb2ef7e3980d02760ea5525e331babb3a4
Chris Harding <chris@transparentux.co.uk>
2025-08-11 12:46:03 +0100

    New GASCD service header
    Combination of the GOV.UK One Login header, the GOV.UK service navigation plus the GOV.UK 'Menu' component.
    
    
    Former-commit-id: fb1a47d076c840053b22e9eb0522b1572edcf6f6

--- app/assets/javascripts/v6/custom-global-behaviour.js
+++ app/assets/javascripts/v6/custom-global-behaviour.js
@@ -6,57 +6,31 @@
 $(document).ready(function () {
 
   // HIDE all non-JS elements
-  $(".filter-non-javascript").hide();
-  $(".filter-non-javascript").addClass("hidden");
-  $(".filter-non-javascript").attr("aria-hidden", true);
-  $(".filter-non-javascript").attr("hidden");
-  $(".expandable-content-non-javascript").hide();
-  $(".expandable-content-non-javascript").addClass("hidden");
-  $(".expandable-content-non-javascript").attr("aria-hidden", true);
-  $(".expandable-content-non-javascript").attr("hidden");
+  $(".non-javascript").attr("aria-hidden", true);
+  $(".non-javascript").attr("hidden", "hidden");
 
   // SHOW any hidden elements which we need for a JavaScript UX
-  $(".filter-with-javascript").show();
-  $(".filter-with-javascript").removeClass("hidden");
-  $(".filter-with-javascript").attr("aria-hidden", false);
-  $(".filter-with-javascript").removeAttr("hidden");
-  $(".expandable-content-with-javascript").show();
-  $(".expandable-content-with-javascript").removeClass("hidden");
-  $(".expandable-content-with-javascript").attr("aria-hidden", false);
-  $(".expandable-content-with-javascript").removeAttr("hidden");
+  $(".javascript-only").attr("aria-hidden", false);
+  $(".javascript-only").removeAttr("hidden", "hidden");
 
-  // On click of the "Filter" header hide the filter section choices on user request
-  $("#headingFilter").click(function () {
+  // On click of the 'Menu' main navigation item, hide open and close the main service 'Menu' section
+  $("#super-navigation-menu-toggle").click(function () {
       
-    var expandFilterSectionState = $(this).attr('aria-expanded');
+    var menuItemState = $(this).attr('aria-expanded');
 
-    if (expandFilterSectionState == "false") {
+    if (menuItemState == "false") {
       // Update the parent (trigger) link
-      $(this).attr("data-expanded", true);
       $(this).attr("aria-expanded", true);
-      $(this).removeClass("filter-icon-closed");
-      $(this).addClass("filter-icon-open");
-      // Then update the child hidden section           
-      $("#filterChoices").show();
-      $("#filterChoices").removeClass("hidden");
-      $("#filterChoices").attr("aria-hidden", false);
-      $("#filterChoices").removeAttr("hidden");
-      $("#filterChoices").css("display", "block");
-      $("#filterChoices").css("margin-top", "0.83333em");
+      // Then open the main service 'Menu' section
+      $("#super-navigation-menu").attr("aria-hidden", false);
+      $("#super-navigation-menu").removeAttr("hidden", "hidden");
     }
-    else if (expandFilterSectionState == "true") {
+    else if (menuItemState == "true") {
       // Update the parent (trigger) link
-      $(this).attr("data-expanded", false);
       $(this).attr("aria-expanded", false);
-      $(this).removeClass("filter-icon-open");
-      $(this).addClass("filter-icon-closed");
       // Then update the child hidden section           
-      $("#filterChoices").hide();
-      $("#filterChoices").addClass("hidden");
-      $("#filterChoices").attr("aria-hidden", true);
-      $("#filterChoices").attr("hidden");
-      $("#filterChoices").css("display", "none");
-      $("#filterChoices").css("margin-top", "0px");
+      $("#super-navigation-menu").attr("aria-hidden", true);
+      $("#super-navigation-menu").attr("hidden", "hidden");
     }
 
   });
@@ -68,18 +42,6 @@ $(document).ready(function () {
 
     // Open the print dialogue
     window.print();
-
-    // Always show the filter section and choices since users need to print these
-    $("#headingFilter").attr("data-expanded", true);
-    $("#headingFilter").attr("aria-expanded", true);
-    $("#headingFilter").removeClass("filter-icon-closed");
-    $("#headingFilter").addClass("filter-icon-open");
-    $("#filterChoices").show();
-    $("#filterChoices").removeClass("hidden");
-    $("#filterChoices").attr("aria-hidden", false);
-    $("#filterChoices").removeAttr("hidden");
-    $("#filterChoices").css("display", "block");
-    $("#filterChoices").css("margin-top", "0.83333em");
   });
 
 });
