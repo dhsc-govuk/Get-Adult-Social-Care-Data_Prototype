b33f9d70f78884a4360bcb5e13dc51e166cf695f
Chris Harding <chris@transparentux.co.uk>
2025-08-19 22:48:07 +0100

    v6 (community social care)
    New designs to continue improving the service header, plus changes to facilitate the new data (community social care).
    
    
    Former-commit-id: e57ec48ca48c1d01ffac80e4976abad9984b5d50

--- app/assets/javascripts/v6/custom-global-behaviour.js
+++ app/assets/javascripts/v6/custom-global-behaviour.js
@@ -13,21 +13,25 @@ $(document).ready(function () {
   $(".javascript-only").attr("aria-hidden", false);
   $(".javascript-only").removeAttr("hidden", "hidden");
 
-  // On click of the 'Menu' main navigation item, hide open and close the main service 'Menu' section
+  // On click of the 'Menu' main navigation item, open and close the main service 'Menu' section
   $("#super-navigation-menu-toggle").click(function () {
-      
+
     var menuItemState = $(this).attr('aria-expanded');
 
     if (menuItemState == "false") {
-      // Update the parent (trigger) link
+      // Update the parent (trigger) button
       $(this).attr("aria-expanded", true);
+      $("#navigation li:last-child").addClass("govuk-service-navigation__item--active");
+      $("#super-navigation-menu-toggle").addClass("gem-c-layout-super-navigation-header__open-button");
       // Then open the main service 'Menu' section
       $("#super-navigation-menu").attr("aria-hidden", false);
       $("#super-navigation-menu").removeAttr("hidden", "hidden");
     }
     else if (menuItemState == "true") {
-      // Update the parent (trigger) link
+      // Update the parent (trigger) button
       $(this).attr("aria-expanded", false);
+      $("#navigation li:last-child").removeClass("govuk-service-navigation__item--active");
+      $("#super-navigation-menu-toggle").removeClass("gem-c-layout-super-navigation-header__open-button");
       // Then update the child hidden section           
       $("#super-navigation-menu").attr("aria-hidden", true);
       $("#super-navigation-menu").attr("hidden", "hidden");
