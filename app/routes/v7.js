e54c247d3684c74cb51cea86688649fa74659e70
Chris Harding <chris@transparentux.co.uk>
2025-09-09 12:25:04 +0100

    New prototype version (v7)
    Created to include designs to address project design debt across all of the service, plus is based on user insights from community social care providers gathered during user research (Round 9: September 2025).
    
    
    Former-commit-id: 4da3fcefe88c3cad174f719a8b429aa0dd4a505e

---
+++ app/routes/v7.js
@@ -0,0 +1,81 @@
+module.exports = function(router) {
+
+  var version = 'v7';
+
+  /*****
+   * General prototype pages (not part of the service)
+  *****/
+
+  router.get('/' + version + '/' + 'config', function (req, res) {
+    res.render(version + '/config', {
+      'version' : version.substring(1)
+		})
+  })
+
+  router.post('/' + version + '/' + 'config-validation', function (req, res) {
+
+    var userType = req.session.data['userType']
+    var entryPoint = req.session.data['entryPoint']
+
+    // For Single Academy Trust (SAT) type users
+    if (userType == "Single Academy Trust (SAT)") {
+      req.session.data['rb'] = "SAT"
+    }
+    // For Multi Academy Trust (MAT) type users
+    else {
+      req.session.data['rb'] = "MAT"
+    }
+
+    // Routing - send to the chosen entry point
+    if (entryPoint == "Start page on GOV.UK (Estate Management Portal)") {
+      res.redirect('/' + version + '/' + 'emp/start')
+    }
+    else if (entryPoint == "Department for Education Sign-in") {
+      res.redirect('/' + version + '/' + 'dfe-sign-in/department-for-education-sign-in')
+    }
+    else if (entryPoint == "Home (Estate Management Portal)") {
+      res.redirect('/' + version + '/' + 'emp/home')
+    }
+    else {
+      res.redirect('/' + version + '/' + 'start')
+    }
+
+  })
+
+  /*****
+   * Not signed in (Get adult social care data)
+   * GOV.UK
+  *****/
+
+  /*****
+   * Signed in (Get adult social care data)
+   * Select location
+  *****/
+
+  router.get('/' + version + '/' + 'signed-in/select-location', function (req, res) {
+    res.render(version + '/signed-in/select-location', {
+      'error' : req.query.error
+		})
+  })
+
+  router.post('/' + version + '/' + 'signed-in/select-location-validation', function (req, res) {
+
+    var careHome = req.body['careHome']
+
+    // Error validation - make sure user enters data into required field
+    if (careHome == undefined) {
+      res.redirect('/' + version + '/' + 'signed-in/select-location?error=true')
+    }
+    // User selects a location
+    else {
+      res.redirect('/' + version + '/' + 'signed-in/home?justSignedIn=false')
+    }
+
+  })
+
+  /*****
+   * Additional screens (Get adult social care data)
+   * ?
+  *****/  
+
+}
