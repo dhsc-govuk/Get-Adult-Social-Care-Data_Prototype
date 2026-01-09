2d8414a8b8cde4f11370bdcc3d72d30e4cbd868f
Chris Harding <chris@transparentux.co.uk>
2026-01-09 16:51:17 +0000

    New table with trend data
    New CT data added for a missing trend table - in v8.
    
    
    Former-commit-id: c978290ab6ae8d50b083bb896e23a71da06be6af

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -5,6 +5,8 @@ module.exports = {
   "v8ProviderLocations": "v8ProviderLocationsData",
   "v8CareProvidersLocationsAndServicesData": require('./v8/residential-care/care-providers-locations-and-services.json'),
   "v8CareProvidersLocationsAndServices": "v8CareProvidersLocationsAndServicesData",
+  "v8CareHomeBedsAndOccupancyData": require('./v8/residential-care/care-home-beds-and-occupancy.json'),
+  "v8CareHomeBedsAndOccupancy": "v8CareHomeBedsAndOccupancyData",
   "v8localAuthorityFundingForAdultSocialCareData": require('./v8/funding/local-authority-funding-for-adult-social-care.json'),
   "v8localAuthorityFundingForAdultSocialCare": "v8localAuthorityFundingForAdultSocialCareData",
   "v8generalHealthDisabilityAndLearningDisabilityData": require('./v8/population-needs/general-health-disability-and-learning-disability.json'),
