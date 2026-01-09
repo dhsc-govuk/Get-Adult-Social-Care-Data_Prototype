2f8a29221af8d4a57408c3ac5f373a06694f2160
Chris Harding <chris@transparentux.co.uk>
2026-01-09 11:39:54 +0000

    Private beta (January 2026)
    Created a new version in the prototype for the private beta to show the development team what designs need to be in that release.
    
    
    Former-commit-id: 6a111cace5f0679a426ef85d2403d743fbe8d9dd

---
+++ app/routes/private-beta/2026/january/routes.js
@@ -0,0 +1,351 @@
+module.exports = function(router) {
+
+  var version = 'private-beta/2026/january';
+  const providerLocations = require('../../../../data/v8/provider-locations.json');
+
+  /*****
+   * General prototype pages (not part of the service)
+  *****/
+
+  router.post('/' + version + '/' + 'config-validation', function (req, res) {
+    
+    var userType = req.session.data['userType']
+    var entryPoint = req.session.data['entryPoint']
+    var numberOfLocations = req.session.data['numberOfLocations']
+
+    // For 'Number of locations' is '1'
+    if (numberOfLocations == "1") {
+
+      if (userType == "Care provider (care home)") {
+        req.session.data['selectedLocationName'] = "Ashfield Court (Exeter)"
+        req.session.data['selectedServiceType'] = "Care home"
+      }
+      else {
+        req.session.data['selectedLocationName'] = "Station Road Centre (Sudbury)"
+        req.session.data['selectedServiceType'] = "Community social care"
+      }
+
+      req.session.data['locations'] = "1"
+      req.session.data['postAuthenticationURL'] = "home"
+    }
+    // For 'Number of locations' is '2 to 20'
+    else if (numberOfLocations == "2 to 20 (select a location)") {
+      req.session.data['locations'] = ""
+      req.session.data['selectedLocationName'] = ""
+      req.session.data['justSignedIn'] = "true"
+      req.session.data['postAuthenticationURL'] = "select-location?searchRequired=false&paginationRequired=false"
+    }
+    // For 'Number of locations' is '21 to 100'
+    else if (numberOfLocations == "21 to 100 (select a location with search)") {
+      req.session.data['locations'] = ""
+      req.session.data['selectedLocationName'] = ""
+      req.session.data['justSignedIn'] = "true"
+      req.session.data['postAuthenticationURL'] = "select-location?searchRequired=true&paginationRequired=false"
+    }
+    // For 'Number of locations' is '101 or more'
+    else {
+      req.session.data['locations'] = ""
+      req.session.data['selectedLocationName'] = ""
+      req.session.data['justSignedIn'] = "true"
+      req.session.data['postAuthenticationURL'] = "select-location?searchRequired=true&paginationRequired=true&page1=true"
+    }
+
+    // Routing - send to the chosen entry point
+    if (entryPoint == "Start page (private beta)") {
+      res.redirect('/' + version + '/' + 'start')
+    }
+    else if (entryPoint == "GOV.UK One Login") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/start')
+    }
+    else {
+      res.redirect('/' + version + '/' + 'signed-in/' + req.session.data['postAuthenticationURL'])
+    }
+
+  })
+
+  /*****
+   * Not signed in
+   * GOV.UK One Login (https://www.sign-in.service.gov.uk)
+  *****/
+
+  /* Create an account */
+  router.get('/' + version + '/' + 'gov-uk/one-login/create-account/get-security-code', function (req, res) {
+    res.render(version + '/gov-uk/one-login/create-account/get-security-code', {
+      'error' : req.query.error
+		})
+  })
+
+  router.post('/' + version + '/' + 'gov-uk/one-login/create-account/get-security-code-validation', function (req, res) {
+
+    var chooseSecurityCodes = req.body['choose-security-codes']
+    
+    if (chooseSecurityCodes == "Text message") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/create-account/enter-phone-number')
+    }
+    else if (chooseSecurityCodes == "Authenticator app for smartphone, tablet or computer") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/create-account/auth-app')
+    }
+    // Error validation - make sure user chooses an option
+    else {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/create-account/get-security-code?error=true')
+    }
+
+  })
+
+  /* Sign in */
+  router.get('/' + version + '/' + 'gov-uk/one-login/sign-in/cannot-change-security-codes', function (req, res) {
+    res.render(version + '/gov-uk/one-login/sign-in/cannot-change-security-codes', {
+      'error' : req.query.error
+		})
+  })
+
+  router.post('/' + version + '/' + 'gov-uk/one-login/sign-in/cannot-change-security-codes-validation', function (req, res) {
+
+    var cannotChangeHowGetSecurityCodeAction = req.body['cannotChangeHowGetSecurityCodeAction']
+    var redirectMFA = req.session.data['redirectMFA'] || "check-phone"
+    
+    if (cannotChangeHowGetSecurityCodeAction == "Try entering a security code again with the method you already have set up") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/' + redirectMFA)
+    }
+    // Error validation - make sure user chooses an option
+    else {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/cannot-change-security-codes?error=true')
+    }
+
+  })
+
+  router.get('/' + version + '/' + 'gov-uk/one-login/sign-in/how-do-you-want-security-codes', function (req, res) {
+    res.render(version + '/gov-uk/one-login/sign-in/how-do-you-want-security-codes', {
+      'error' : req.query.error
+		})
+  })
+
+  router.post('/' + version + '/' + 'gov-uk/one-login/sign-in/how-do-you-want-security-codes-validation', function (req, res) {
+
+    var mfaMethodId = req.body['mfa-method-id']
+    
+    if (mfaMethodId == "Use your authenticator app") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/enter-authenticator-app-code')
+    }
+    else if (mfaMethodId == "Text message") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/check-phone')
+    }
+    // Error validation - make sure user chooses an option
+    else {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/how-do-you-want-security-codes?error=true')
+    }
+
+  })
+
+  /* I’ve forgotten my password */
+  router.get('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/cannot-change-security-codes', function (req, res) {
+    res.render(version + '/gov-uk/one-login/sign-in/forgotten-password/cannot-change-security-codes', {
+      'error' : req.query.error
+		})
+  })
+
+  router.post('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/cannot-change-security-codes-validation', function (req, res) {
+
+    var cannotChangeHowGetSecurityCodeAction = req.body['cannotChangeHowGetSecurityCodeAction']
+    var redirectMFA = req.session.data['redirectMFA'] || "check-phone"
+    
+    if (cannotChangeHowGetSecurityCodeAction == "Try entering a security code again with the method you already have set up") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/' + redirectMFA)
+    }
+    // Error validation - make sure user chooses an option
+    else {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/cannot-change-security-codes?error=true')
+    }
+
+  })
+
+  router.get('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/how-do-you-want-security-codes', function (req, res) {
+    res.render(version + '/gov-uk/one-login/sign-in/forgotten-password/how-do-you-want-security-codes', {
+      'error' : req.query.error
+		})
+  })
+
+  router.post('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/how-do-you-want-security-codes-validation', function (req, res) {
+
+    var mfaMethodId = req.body['mfa-method-id']
+    
+    if (mfaMethodId == "Use your authenticator app") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/enter-authenticator-app-code')
+    }
+    else if (mfaMethodId == "Text message") {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/check-phone')
+    }
+    // Error validation - make sure user chooses an option
+    else {
+      res.redirect('/' + version + '/' + 'gov-uk/one-login/sign-in/forgotten-password/how-do-you-want-security-codes?error=true')
+    }
+
+  })
+
+  /*****
+   * Signed in
+   * Select location
+  *****/
+
+  router.get('/' + version + '/' + 'signed-in/select-location', function (req, res) {
+    res.render(version + '/signed-in/select-location', {
+      'error' : req.query.error,
+      'error1' : req.query.error1,
+      'error2' : req.query.error2,
+      'page1' : req.query.page1,
+      'page2' : req.query.page2,
+      'searchLocation' : req.query.searchLocation,
+      'updateMyLocation' : req.query.updateMyLocation
+		})
+  })
+  // NON JAVASCRIPT - Deal with the non JavaScript scenario (e.g. POST) for users entering an empty search on the radio buttons
+	router.get('/' + version + '/' + 'signed-in/select-location-non-javascript-post', function (req, res) {		
+
+    var searchLocation = req.query['searchLocation']
+
+		// Error validation - make sure user enters a search term
+		if (searchLocation == "") {
+			res.redirect('/' + version + '/' + 'signed-in/select-location?searchRequired=true&paginationRequired=true&page1=true&error=true&error1=true')
+		}
+		// User searches for a location
+		else {
+			res.redirect('/' + version + '/' + 'signed-in/select-location?searchRequired=true&paginationRequired=true&page1=true')
+		}
+		
+	})
+  router.post('/' + version + '/' + 'signed-in/select-location-validation', function (req, res) {
+
+    // Data objects to be retrieved and queried
+    var locationId = req.body['locationId']
+
+    // Error validation - make sure user enters data into required field
+		if (locationId == undefined) {
+      res.redirect('/' + version + '/' + 'signed-in/select-location?error=true&error2=true')      
+		}
+		// User selects a location
+		else {		
+      
+      // Take user's selection ID, query it in the provider locations JSON data and store the related data objects as session data
+      var locationId = Number(req.body['locationId'])
+      const selectedLocation = providerLocations.find(l => l.ID === locationId)
+      req.session.data['selectedLocation'] = selectedLocation
+      req.session.data['selectedProviderName'] = selectedLocation?.['Provider name']
+      req.session.data['selectedLocationName'] = selectedLocation?.['Location name']
+      req.session.data['selectedLocationAddress'] = selectedLocation?.['Address']
+      req.session.data['selectedLocationPostcode'] = selectedLocation?.['Postcode']
+      req.session.data['selectedServiceType'] = selectedLocation?.['Service type']
+
+      res.redirect('/' + version + '/' + 'signed-in/home?justSignedIn=false')
+    }
+
+  })
+  router.post('/' + version + '/' + 'signed-in/select-location-search-and-pagination-validation', function (req, res) {
+
+    // Data objects to be retrieved and queried
+    var searchRequired = req.session.data['searchRequired']
+    var paginationRequired = req.session.data['paginationRequired']
+    var locationId = req.body['locationId']
+
+    // Logic and validation for routing
+    var searchRequiredURL = "";
+    var paginationRequiredURL = "";
+    
+    if (searchRequired == "true") {
+      searchRequiredURL = "&searchRequired=true"
+    }
+
+    if (paginationRequired == "true") {
+      paginationRequiredURL = "&paginationRequired=true&page1=true"
+    }
+
+    // Error validation - make sure user enters data into required field
+		if (locationId == undefined) {
+      res.redirect('/' + version + '/' + 'signed-in/select-location?error=true&error2=true' + searchRequiredURL + paginationRequiredURL)      
+		}
+		// User selects a location
+		else {
+
+      // Take user's selection ID, query it in the provider locations JSON data and store the related data objects as session data
+      var locationId = Number(req.body['locationId'])
+      const selectedLocation = providerLocations.find(l => l.ID === locationId)
+      req.session.data['selectedLocation'] = selectedLocation
+      req.session.data['selectedProviderName'] = selectedLocation?.['Provider name']
+      req.session.data['selectedLocationName'] = selectedLocation?.['Location name']
+      req.session.data['selectedLocationAddress'] = selectedLocation?.['Address']
+      req.session.data['selectedLocationPostcode'] = selectedLocation?.['Postcode']
+      req.session.data['selectedServiceType'] = selectedLocation?.['Service type']
+
+      res.redirect('/' + version + '/' + 'signed-in/home?justSignedIn=false')
+    }
+
+  })
+
+  /*****
+   * Signed in
+   * Data > Care provision
+  *****/
+
+  // Care home beds and occupancy levels
+  router.post('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data-update-filters1', function (req, res) {
+
+    // Data objects to be retrieved and queried
+    var bedType1 = req.session.data['bedType1']
+
+    // Reset all default values injected into our filters
+    if (bedType1 == "All bed types") {
+      bedType1 = undefined
+    }
+
+    // User has chosen at least 1 filter  
+		if (bedType1) {
+      res.redirect('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data?filterApplied1=Yes#data1')     
+		}
+		// No filters selected by user
+		else {			
+      res.redirect('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data?filterApplied1=#data1')
+    }
+    
+  })
+  router.post('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data-update-filters2', function (req, res) {
+
+    // Data objects to be retrieved and queried
+    var bedType2 = req.session.data['bedType2']
+
+    // User has chosen at least 1 filter  
+		if (bedType2) {
+      res.redirect('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data?filterApplied2=Yes#data2')     
+		}
+		// No filters selected by user
+		else {			
+      res.redirect('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data?filterApplied2=#data2')
+    }
+    
+  })
+  router.post('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data-update-filters3', function (req, res) {
+
+    // Data objects to be retrieved and queried
+    var bedType3 = req.session.data['bedType3']
+
+    // User has chosen at least 1 filter  
+		if (bedType3) {
+      res.redirect('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data?filterApplied3=Yes#trend1')     
+		}
+		// No filters selected by user
+		else {			
+      res.redirect('/' + version + '/' + 'signed-in/topics/residential-care/provision-and-occupancy/data?filterApplied3=#trend1')
+    }
+    
+  })
+
+  /*****
+   * Additional screens
+   * Footer, service information and system pages
+  *****/
+
+  router.get('/' + version + '/' + 'footer/cookies', function (req, res) {
+    res.render(version + '/footer/cookies', {
+      'cookiesUpdated' : req.query.cookiesUpdated
+		})
+  })
+
+}
