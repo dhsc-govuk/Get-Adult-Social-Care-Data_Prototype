f490f9c757295d281318549acf76f8b34b60c0e8
Matthew Sital-Singh <matthew.sitalsingh@madetech.com>
2026-01-21 09:39:47 +0000

    Attempt to work around Azure's URL encoding issues
    Former-commit-id: eb370a3a30951bd3b374ebdd09c5a6f0a954389f

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,24 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+// Workaround for Azure App Service URL encoding
+// https://github.com/Azure/azure-functions-host/pull/9402
+router.use((req, res, next) => {
+  // Azure App Service stores the raw URL in this header
+  const rawUrl = req.headers['x-waws-unencoded-url']
+  
+  if (rawUrl) {
+    // rawUrl includes the query string (e.g. /public/images/@user.png?v=1)
+    const urlParts = rawUrl.split('?')
+    
+    // Override the request properties so Express can find the real file
+    req.url = rawUrl
+    req.originalUrl = rawUrl
+    req.path = urlParts[0]
+  }
+  next()
+})
+
 // Versioned route files
 require('./routes/private-beta/2026/january/routes')(router)
 require('./routes/v8')(router)
